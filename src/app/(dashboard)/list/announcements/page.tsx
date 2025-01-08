740dafb9dd794dd88fb7d2c85bb20c2398fb7b00
manish <manishrajput6196@gmail.com>
2025-01-08 16:57:32 +0530

    feat: integration - announcements & assignments tables

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -2,62 +2,23 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/settings';
+import { auth } from '@clerk/nextjs/server';
 import { Announcement, Class, Prisma } from '@prisma/client';
 import Image from 'next/image';
 
 type AnnouncementListItem = Announcement & { class: Class };
-
-const columns = [
-  {
-    header: 'Title',
-    accessor: 'title',
-  },
-  {
-    header: 'Class',
-    accessor: 'class',
-  },
-  {
-    header: 'Date',
-    accessor: 'date',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
-];
-
-const renderRow = (item: AnnouncementListItem) => (
-  <tr
-    key={item.id}
-    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
-  >
-    <td className='flex items-center gap-4 p-4'>{item.title}</td>
-    <td>{item.class?.name || '-'}</td>
-    <td className='hidden md:table-cell'>
-      {new Intl.DateTimeFormat('en-US').format(item.date)}
-    </td>
-    <td>
-      <div className='flex items-center gap-2'>
-        {role === 'admin' && (
-          <>
-            <FormModal table='announcement' type='update' data={item} />
-            <FormModal table='announcement' type='delete' id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
 
 const AnnouncementListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { userId, sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
   const { page, ...queryParams } = await searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -82,18 +43,18 @@ const AnnouncementListPage = async ({
 
   // ROLE CONDITIONS
 
-  // const roleConditions = {
-  //   teacher: { lessons: { some: { teacherId: currentUserId! } } },
-  //   student: { students: { some: { id: currentUserId! } } },
-  //   parent: { students: { some: { parentId: currentUserId! } } },
-  // };
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
 
-  // query.OR = [
-  //   { classId: null },
-  //   {
-  //     class: roleConditions[role as keyof typeof roleConditions] || {},
-  //   },
-  // ];
+  query.OR = [
+    { classId: null },
+    {
+      class: roleConditions[role as keyof typeof roleConditions] || {},
+    },
+  ];
 
   const [data, count] = await prisma.$transaction([
     prisma.announcement.findMany({
@@ -106,6 +67,53 @@ const AnnouncementListPage = async ({
     }),
     prisma.announcement.count({ where: query }),
   ]);
+
+  const columns = [
+    {
+      header: 'Title',
+      accessor: 'title',
+    },
+    {
+      header: 'Class',
+      accessor: 'class',
+    },
+    {
+      header: 'Date',
+      accessor: 'date',
+      className: 'hidden md:table-cell',
+    },
+    ...(role === 'admin'
+      ? [
+          {
+            header: 'Actions',
+            accessor: 'action',
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: AnnouncementListItem) => (
+    <tr
+      key={item.id}
+      className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
+    >
+      <td className='flex items-center gap-4 p-4'>{item.title}</td>
+      <td>{item.class?.name || '-'}</td>
+      <td className='hidden md:table-cell'>
+        {new Intl.DateTimeFormat('en-US').format(item.date)}
+      </td>
+      <td>
+        <div className='flex items-center gap-2'>
+          {role === 'admin' && (
+            <>
+              <FormModal table='announcement' type='update' data={item} />
+              <FormModal table='announcement' type='delete' id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
