7f364e99df31812760b69b0e5adac4b32a2949ef
manish <manishrajput6196@gmail.com>
2025-01-08 17:10:29 +0530

    feat: integration - exams, lessons, parents, results tables

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -2,9 +2,9 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/settings';
+import { auth } from '@clerk/nextjs/server';
 import { Class, Lesson, Prisma, Subject, Teacher } from '@prisma/client';
 import Image from 'next/image';
 
@@ -12,52 +12,14 @@ type LessonListItems = Lesson & { subject: Subject } & { class: Class } & {
   teacher: Teacher;
 };
 
-const columns = [
-  {
-    header: 'Subject Name',
-    accessor: 'name',
-  },
-  {
-    header: 'Class',
-    accessor: 'class',
-  },
-  {
-    header: 'Teacher',
-    accessor: 'teacher',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
-];
-
-const renderRow = (item: LessonListItems) => (
-  <tr
-    key={item.id}
-    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
-  >
-    <td className='flex items-center gap-4 p-4'>{item.subject.name}</td>
-    <td>{item.class.name}</td>
-    <td className='hidden md:table-cell'>{item.teacher.name}</td>
-    <td>
-      <div className='flex items-center gap-2'>
-        {role === 'admin' && (
-          <>
-            <FormModal table='lesson' type='update' data={item} />
-            <FormModal table='lesson' type='delete' id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
-
 const LessonListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+
   const { page, ...queryParams } = await searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -102,6 +64,51 @@ const LessonListPage = async ({
     }),
     prisma.lesson.count({ where: query }),
   ]);
+
+  const columns = [
+    {
+      header: 'Subject Name',
+      accessor: 'name',
+    },
+    {
+      header: 'Class',
+      accessor: 'class',
+    },
+    {
+      header: 'Teacher',
+      accessor: 'teacher',
+      className: 'hidden md:table-cell',
+    },
+    ...(role === 'admin'
+      ? [
+          {
+            header: 'Actions',
+            accessor: 'action',
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: LessonListItems) => (
+    <tr
+      key={item.id}
+      className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
+    >
+      <td className='flex items-center gap-4 p-4'>{item.subject.name}</td>
+      <td>{item.class.name}</td>
+      <td className='hidden md:table-cell'>{item.teacher.name}</td>
+      <td>
+        <div className='flex items-center gap-2'>
+          {role === 'admin' && (
+            <>
+              <FormModal table='lesson' type='update' data={item} />
+              <FormModal table='lesson' type='delete' id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -2,9 +2,9 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/settings';
+import { auth } from '@clerk/nextjs/server';
 import { Prisma } from '@prisma/client';
 import Image from 'next/image';
 
@@ -20,75 +20,15 @@ type ResultListItem = {
   startTime: Date;
 };
 
-const columns = [
-  {
-    header: 'Subject Name',
-    accessor: 'name',
-  },
-  {
-    header: 'Student',
-    accessor: 'student',
-  },
-  {
-    header: 'Score',
-    accessor: 'score',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Teacher',
-    accessor: 'teacher',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Class',
-    accessor: 'class',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Date',
-    accessor: 'date',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
-];
-
-const renderRow = (item: ResultListItem) => (
-  <tr
-    key={item.id}
-    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
-  >
-    <td className='flex items-center gap-4 p-4'>{item.title}</td>
-    <td>{item.studentName + ' ' + item.studentName}</td>
-    <td className='hidden md:table-cell'>{item.score}</td>
-    <td className='hidden md:table-cell'>
-      {item.teacherName + ' ' + item.teacherSurname}
-    </td>
-    <td className='hidden md:table-cell'>{item.className}</td>
-    <td className='hidden md:table-cell'>
-      {new Intl.DateTimeFormat('en-US').format(item.startTime)}
-    </td>
-    <td>
-      <div className='flex items-center gap-2'>
-        {role === 'admin' ||
-          (role === 'teacher' && (
-            <>
-              <FormModal table='result' type='update' data={item} />
-              <FormModal table='result' type='delete' id={item.id} />
-            </>
-          ))}
-      </div>
-    </td>
-  </tr>
-);
-
 const ResultListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { userId, sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
   const { page, ...queryParams } = await searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -122,22 +62,22 @@ const ResultListPage = async ({
   switch (role) {
     case 'admin':
       break;
-    // case 'teacher':
-    //   query.OR = [
-    //     { exam: { lesson: { teacherId: currentUserId! } } },
-    //     { assignment: { lesson: { teacherId: currentUserId! } } },
-    //   ];
-    //   break;
+    case 'teacher':
+      query.OR = [
+        { exam: { lesson: { teacherId: currentUserId! } } },
+        { assignment: { lesson: { teacherId: currentUserId! } } },
+      ];
+      break;
 
-    // case 'student':
-    //   query.studentId = currentUserId!;
-    //   break;
+    case 'student':
+      query.studentId = currentUserId!;
+      break;
 
-    // case 'parent':
-    //   query.student = {
-    //     parentId: currentUserId!,
-    //   };
-    //   break;
+    case 'parent':
+      query.student = {
+        parentId: currentUserId!,
+      };
+      break;
 
     default:
       break;
@@ -194,6 +134,74 @@ const ResultListPage = async ({
       startTime: isExam ? assessment.startTime : assessment.startDate,
     };
   });
+
+  const columns = [
+    {
+      header: 'Subject Name',
+      accessor: 'name',
+    },
+    {
+      header: 'Student',
+      accessor: 'student',
+    },
+    {
+      header: 'Score',
+      accessor: 'score',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Teacher',
+      accessor: 'teacher',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Class',
+      accessor: 'class',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Date',
+      accessor: 'date',
+      className: 'hidden md:table-cell',
+    },
+    ...(role === 'admin' || role === 'teacher'
+      ? [
+          {
+            header: 'Actions',
+            accessor: 'action',
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: ResultListItem) => (
+    <tr
+      key={item.id}
+      className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
+    >
+      <td className='flex items-center gap-4 p-4'>{item.title}</td>
+      <td>{item.studentName + ' ' + item.studentName}</td>
+      <td className='hidden md:table-cell'>{item.score}</td>
+      <td className='hidden md:table-cell'>
+        {item.teacherName + ' ' + item.teacherSurname}
+      </td>
+      <td className='hidden md:table-cell'>{item.className}</td>
+      <td className='hidden md:table-cell'>
+        {new Intl.DateTimeFormat('en-US').format(item.startTime)}
+      </td>
+      <td>
+        <div className='flex items-center gap-2'>
+          {role === 'admin' ||
+            (role === 'teacher' && (
+              <>
+                <FormModal table='result' type='update' data={item} />
+                <FormModal table='result' type='delete' id={item.id} />
+              </>
+            ))}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
