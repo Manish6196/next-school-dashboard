fc2d8fe05c48297586aef4d9179f0f41185c7515
manish <manishrajput6196@gmail.com>
2025-01-08 17:02:55 +0530

    feat: integration - classes & events tables

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -2,69 +2,22 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/settings';
+import { auth } from '@clerk/nextjs/server';
 import { Class, Prisma, Teacher } from '@prisma/client';
 import Image from 'next/image';
 
 type ClassListItem = Class & { supervisor: Teacher };
-
-const columns = [
-  {
-    header: 'Class Name',
-    accessor: 'name',
-  },
-  {
-    header: 'Capacity',
-    accessor: 'capacity',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Grade',
-    accessor: 'grade',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Supervisor',
-    accessor: 'supervisor',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
-];
-
-const renderRow = (item: ClassListItem) => (
-  <tr
-    key={item.id}
-    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
-  >
-    <td className='flex items-center gap-4 p-4'>{item.name}</td>
-    <td className='hidden md:table-cell'>{item.capacity}</td>
-    <td className='hidden md:table-cell'>{item.name[0]}</td>
-    <td className='hidden md:table-cell'>
-      {item.supervisor.name + ' ' + item.supervisor.surname}
-    </td>
-    <td>
-      <div className='flex items-center gap-2'>
-        {role === 'admin' && (
-          <>
-            <FormModal table='class' type='update' data={item} />
-            <FormModal table='class' type='delete' id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
 
 const ClassListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+
   const { page, ...queryParams } = await searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -101,6 +54,60 @@ const ClassListPage = async ({
     }),
     prisma.class.count({ where: query }),
   ]);
+
+  const columns = [
+    {
+      header: 'Class Name',
+      accessor: 'name',
+    },
+    {
+      header: 'Capacity',
+      accessor: 'capacity',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Grade',
+      accessor: 'grade',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Supervisor',
+      accessor: 'supervisor',
+      className: 'hidden md:table-cell',
+    },
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: ClassListItem) => (
+    <tr
+      key={item.id}
+      className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
+    >
+      <td className='flex items-center gap-4 p-4'>{item.name}</td>
+      <td className='hidden md:table-cell'>{item.capacity}</td>
+      <td className='hidden md:table-cell'>{item.name[0]}</td>
+      <td className='hidden md:table-cell'>
+        {item.supervisor.name + ' ' + item.supervisor.surname}
+      </td>
+      <td>
+        <div className='flex items-center gap-2'>
+          {role === 'admin' && (
+            <>
+              <FormModal table='class' type='update' data={item} />
+              <FormModal table='class' type='delete' id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -2,86 +2,23 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/settings';
+import { auth } from '@clerk/nextjs/server';
 import { Class, Event, Prisma } from '@prisma/client';
 import Image from 'next/image';
 
 type EventListItem = Event & { class: Class };
-
-const columns = [
-  {
-    header: 'Title',
-    accessor: 'title',
-  },
-  {
-    header: 'Class',
-    accessor: 'class',
-  },
-  {
-    header: 'Date',
-    accessor: 'date',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Start Time',
-    accessor: 'startTime',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'End Time',
-    accessor: 'endTime',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
-];
-
-const renderRow = (item: EventListItem) => (
-  <tr
-    key={item.id}
-    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
-  >
-    <td className='flex items-center gap-4 p-4'>{item.title}</td>
-    <td>{item.class?.name || '-'}</td>
-    <td className='hidden md:table-cell'>
-      {new Intl.DateTimeFormat('en-US').format(item.startTime)}
-    </td>
-    <td className='hidden md:table-cell'>
-      {item.startTime.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-      })}
-    </td>
-    <td className='hidden md:table-cell'>
-      {item.endTime.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-      })}
-    </td>
-    <td>
-      <div className='flex items-center gap-2'>
-        {role === 'admin' && (
-          <>
-            <FormModal table='event' type='update' data={item} />
-            <FormModal table='event' type='delete' id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
 
 const EventListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { userId, sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
   const { page, ...queryParams } = await searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -106,18 +43,18 @@ const EventListPage = async ({
 
   // ROLE CONDITIONS
 
-  // const roleConditions = {
-  //   teacher: { lessons: { some: { teacherId: currentUserId! } } },
-  //   student: { students: { some: { id: currentUserId! } } },
-  //   parent: { students: { some: { parentId: currentUserId! } } },
-  // };
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
 
-  // query.OR = [
-  //   { classId: null },
-  //   {
-  //     class: roleConditions[role as keyof typeof roleConditions] || {},
-  //   },
-  // ];
+  query.OR = [
+    { classId: null },
+    {
+      class: roleConditions[role as keyof typeof roleConditions] || {},
+    },
+  ];
 
   const [data, count] = await prisma.$transaction([
     prisma.event.findMany({
@@ -130,6 +67,77 @@ const EventListPage = async ({
     }),
     prisma.event.count({ where: query }),
   ]);
+
+  const columns = [
+    {
+      header: 'Title',
+      accessor: 'title',
+    },
+    {
+      header: 'Class',
+      accessor: 'class',
+    },
+    {
+      header: 'Date',
+      accessor: 'date',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Start Time',
+      accessor: 'startTime',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'End Time',
+      accessor: 'endTime',
+      className: 'hidden md:table-cell',
+    },
+    ...(role === 'admin'
+      ? [
+          {
+            header: 'Actions',
+            accessor: 'action',
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: EventListItem) => (
+    <tr
+      key={item.id}
+      className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
+    >
+      <td className='flex items-center gap-4 p-4'>{item.title}</td>
+      <td>{item.class?.name || '-'}</td>
+      <td className='hidden md:table-cell'>
+        {new Intl.DateTimeFormat('en-US').format(item.startTime)}
+      </td>
+      <td className='hidden md:table-cell'>
+        {item.startTime.toLocaleTimeString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        })}
+      </td>
+      <td className='hidden md:table-cell'>
+        {item.endTime.toLocaleTimeString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        })}
+      </td>
+      <td>
+        <div className='flex items-center gap-2'>
+          {role === 'admin' && (
+            <>
+              <FormModal table='event' type='update' data={item} />
+              <FormModal table='event' type='delete' id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
